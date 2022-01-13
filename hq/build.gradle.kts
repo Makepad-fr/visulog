2c472ddc72a85cacd207ed4ca4e7f3080d32d946
idilsaglam <idilsaglam1@hotmail.com>
2022-01-13 04:21:35 +0100

    feat(hq): module added

---
+++ hq/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java-library")
+}
+
+group = "up.visulog"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
