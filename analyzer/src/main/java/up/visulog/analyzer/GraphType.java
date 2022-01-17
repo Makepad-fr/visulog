00640d466e34ff78c4d270273cf2edec9fa00a88
idilsaglam <idilsaglam1@hotmail.com>
2022-01-17 01:44:37 +0100

    feat(analyzer): New method added to the GraphType enumeration
    A new method to convert a String to a GraphType value implemented

--- analyzer/src/main/java/up/visulog/analyzer/GraphType.java
+++ analyzer/src/main/java/up/visulog/analyzer/GraphType.java
@@ -3,6 +3,21 @@
 */
 package up.visulog.analyzer;
 
+import java.util.Locale;
+
 public enum GraphType {
     BarChart;
+
+    /**
+     * Creates a GraphType from a String
+     * @param string The input string value
+     * @return The GraphType value related to the given string
+     * @throws IllegalArgumentException If the given string matches with any GraphType values
+     */
+    public static GraphType fromString(String string) throws IllegalArgumentException {
+        return switch (string.trim().toLowerCase(Locale.ROOT)) {
+            case "barchart" -> BarChart;
+            default -> throw new IllegalArgumentException(String.format("%s is not a valid option for GraphType enumeration", string));
+        };
+    }
 }
