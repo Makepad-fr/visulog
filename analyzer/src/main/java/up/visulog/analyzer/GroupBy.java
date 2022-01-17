47c3b77ed1a336635db36d38d067f9ffe6022059
idilsaglam <idilsaglam1@hotmail.com>
2022-01-17 01:37:19 +0100

    feat(analyzer): GroupBy enumeration updated
    A new static method implemented to convert a String to a GroupBy value

--- analyzer/src/main/java/up/visulog/analyzer/GroupBy.java
+++ analyzer/src/main/java/up/visulog/analyzer/GroupBy.java
@@ -3,6 +3,8 @@
 */
 package up.visulog.analyzer;
 
+import java.util.Locale;
+
 public enum GroupBy {
     Hour,
     Day,
@@ -10,4 +12,24 @@ public enum GroupBy {
     Month,
     Quarter,
     Year;
+
+    /**
+     * Converts the given String to the GroupBy value if possible
+     *
+     * @param string The string to convert
+     * @return GroupBy value related to the given string
+     * @throws IllegalArgumentException If the given string does not match with any GroupBy values
+     */
+    public static GroupBy fromString(String string) throws IllegalArgumentException {
+        return switch (string.trim().toLowerCase(Locale.ROOT)) {
+            case "hour" -> Hour;
+            case "day" -> Day;
+            case "week" -> Week;
+            case "month" -> Month;
+            case "quarter" -> Quarter;
+            case "year" -> Year;
+            default -> throw new IllegalArgumentException(
+                    String.format("%s is not a vlid groupBy type", string));
+        };
+    }
 }
